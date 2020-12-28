81a0ce45d14a34c91fc9e457a9800f59860bdd07
Łukasz Bacik <mail@luka.sh>
2020-12-28 16:31:40 +0100

    minor changes in the footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {version} from '../package.json';
+
+const fortuneUrl = process.env.REACT_APP_FORTUNE_URL || 'http://localhost:8080'
+
+ReactDOM.render(
+    <div>
+        API: <a href={fortuneUrl}>{fortuneUrl}</a>
+    </div>,
+    document.getElementById('api-url'));
+
+document.getElementById('version').innerText = `version: ${version}`
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
