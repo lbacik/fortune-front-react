77110fafa610d590df5afea735ea776bbb8617ec
Łukasz Bacik <mail@luka.sh>
2021-02-06 19:40:41 +0100

    updated explorer toggle component

--- src/components/ExplorerToggle/ExplorerToggle.js
+++ src/components/ExplorerToggle/ExplorerToggle.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import './ExplorerToggle.css'
+import { connect } from 'react-redux'
 
-const ExplorerToggle = (props) => (
-    <div onClick={props.onClick}>{props.explorerShow ? '[ --- close --- ]' : '[ > explore < ]'}</div>
-)
+const ExplorerToggle = (props) => {
+    let classes = ['px-2']
 
-export default ExplorerToggle
+    let button
+    if (props.explorerShow === true) {
+        button = '<<< close'
+    }  else {
+        button = '>>> explore'
+    }
+
+    return (
+        <div id="explorerToggle" className={classes.join(' ')} onClick={props.onExploreToggle}>{button}</div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        explorerShow: state.explorerShow,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onExploreToggle: () => dispatch({type: 'EXPLORER_TOGGLE'}),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExplorerToggle)
