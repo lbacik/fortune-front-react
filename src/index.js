a6e249f857d89cc07a7a26218290c292c427c648
Łukasz Bacik <mail@luka.sh>
2021-02-06 19:42:06 +0100

    updated app (api 0.3, redux)

--- src/index.js
+++ src/index.js
@@ -4,16 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {version} from '../package.json';
+import { createStore } from 'redux'
+import reducer from './store/reducer'
+import { Provider } from 'react-redux'
 
 const fortuneUrl = process.env.REACT_APP_FORTUNE_URL || 'http://localhost:8080'
+const store = createStore(reducer)
 
 ReactDOM.render(
     <div>
         API: <a href={fortuneUrl}>{fortuneUrl}</a>
     </div>,
-    document.getElementById('api-url'));
+    document.getElementById('api-url')
+)
 
 document.getElementById('version').innerText = `version: ${version}`
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+registerServiceWorker()
